560f8b530adb0688a9f432b8952ec6beefce0d0d
thomas-yv <thomas.lessieu@ynov.com>
2025-10-10 11:46:22 +0200

    [NEW] - Added ROOMs and 404 page

--- src/server/handlers/landing_page.go
+++ src/server/handlers/landing_page.go
@@ -2,62 +2,15 @@ package handlers
 
 import (
 	"net/http"
-	"power4web/src/server/data"
-	"power4web/src/server/landing"
-	"strconv"
+	"power4web/src/server/pages"
 )
 
 func LandingPageHandler(w http.ResponseWriter, r *http.Request) {
-	landing.LoadLanding(w, r)
-}
-
-func ChangeSlotHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		pages.LoadPage(w, r, "src/client/404.html")
 		return
 	}
 
-	selectedSlotColor := r.FormValue("change_slot")
-
-	if selectedSlotColor == "" {
-		http.Error(w, "Data sent is malformed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	if data.IsValidColor(selectedSlotColor) {
-		if !data.IsColorTaken(selectedSlotColor, data.ServerData.PlayerSelectedIndex+1) {
-			data.ServerData.Players[data.ServerData.PlayerSelectedIndex].Slot = selectedSlotColor
-
-			http.Redirect(w, r, "/", http.StatusSeeOther)
-		} else {
-			http.Error(w, "This color is already taken by another player", http.StatusMethodNotAllowed)
-			return
-		}
-	} else {
-		http.Error(w, "Invalid color", http.StatusMethodNotAllowed)
-		return
-	}
-}
-
-func SelectPlayerHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	player := r.FormValue("select-player_btn")
-	playerC, err := strconv.Atoi(player)
-	if err != nil {
-		http.Error(w, "Data sent is malformed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	if playerC <= 0 || playerC > data.NumOfPlayers {
-		http.Error(w, "Data sent is malformed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	data.ServerData.PlayerSelectedIndex = playerC - 1
-
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+	pages.LoadPage(w, r, "src/client/index.html")
 }
